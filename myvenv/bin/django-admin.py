3642947cc7f15e23232561e2760aa17b906f99ed
viniciusalencar <viniciusalencar176@gmail.com>
2023-07-19 20:39:57 -0300

    fix depedences

--- myvenv/bin/django-admin.py
+++ myvenv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/user/Desktop/Vinicius/djangogirls/myvenv/bin/python
+#!/Users/user/Desktop/Vinicius/djangogirls/myvenv/bin/python3.10
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
